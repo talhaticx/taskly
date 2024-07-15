3b2fe2ad11d78f9bbf72396ddd7a092505f804a4
Talha Ayyaz <mtalhaayaz2005@gmail.com>
2024-07-15 23:34:17 +0500

    escape removed

--- src/_utils/__init__.py
+++ src/_utils/__init__.py
@@ -2,12 +2,10 @@ from ._rich import console, Padding
 from ._print import printMenu, printList, printTask
 from .extras import wait_for_enter, clear_terminal, caution, delete_file
 from ._settings import Settings
-from .escape import escape
 from ._cooltext import cool_text
 
 __all__ = [
     "cool_text",
-    "escape",
     "console",
     "Padding",
     "printMenu",
