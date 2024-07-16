090e493ffbd8993e46b39aa580fdfd25288145dd
Talha Ayyaz <mtalhaayaz2005@gmail.com>
2024-07-16 16:05:00 +0500

    praser added

--- src/todos/main.py
+++ src/todos/main.py
@@ -1,20 +1,27 @@
-from rich import print
-from rich.layout import Layout
-from rich.panel import Panel
-
-from _utils import *
+import argparse
+import subprocess
+from _utils import clear_terminal, cool_text
 from todos import app
-
+from Version.__init__ import VERSION
 
 def main():
+    parser = argparse.ArgumentParser(description="Task management CLI tool.")
+    parser.add_argument('-v', '--version', action='version', version=f'%(prog)s {VERSION}',
+                        help="Show program's version number and exit.")
+    parser.add_argument('--reset', action='store_true', help='Reset the application state.')
+
+    args = parser.parse_args()
+
+    if args.reset:
+        subprocess.run(["python3", "scripts/reset.py"])
+        return
+
     clear_terminal()
     cool_text("TASKLY")
-    """main function"""
     while True:
         done = app()
         if done == 0:
             break
-
 
 if __name__ == "__main__":
     main()
