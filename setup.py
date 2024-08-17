23f59ec43e8490497b1ed0312872eba27c455d85
talhaticx <mtalhaayaz2005@gmail.com>
2024-08-17 23:34:11 +0500

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 if __name__ == "__main__":
     try:
         setup(
-            version='0.1.4',
+            version='0.1.5',
             use_scm_version={"version_scheme": "no-guess-dev"},
             # packages=find_packages(where='src'),
             # package_dir={'': 'src'},
